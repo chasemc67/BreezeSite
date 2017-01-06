0b9e8ca406d5934b7a5bcf86f3e9ce001010051c
chasemc67 <chasemc67@gmail.com>
2017-01-06 13:13:52 -0700

    contact component in place and styled, ready to be filled in properly to be made working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ContactComponent from './ContactComponent';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ContactComponent />, document.getElementById('contact'));
