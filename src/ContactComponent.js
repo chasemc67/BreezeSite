3445e9066a1deda5cd0b58cad679c0ff2d172330
chasemc67 <chasemc67@gmail.com>
2017-01-07 15:02:11 -0700

    Contact us is visually working now

--- src/ContactComponent.js
+++ src/ContactComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+
+
 export default class ContactComponent extends Component {
   render() {
     return (
@@ -13,29 +15,28 @@ export default class ContactComponent extends Component {
         <div className = "row">
           <div className="twelve columns" style={{marginBottom: "10%"}}>
             <div id="contact-form">
+
+            <label for="name">Name: <span class="required">*</span></label>
+            <input type="text" id="name" name="name" placeholder="John Smith" value="" />
+
+            <label for="email">Email Address: <span class="required">*</span></label>
+            <input type="email" id="email" name="email" value="" placeholder="johnsmith@example.com" required="required" />
+
+            <label for="telephone">Telephone: </label>
+            <input type="tel" id="telephone" name="telephone" placeholder="(780) 444 4444" value="" />
+
+            <label for="contactType">Preferred Mode of Contact: </label>
+            <select id="contactType" name="contactType" >
+                <option value="email">Email</option>
+                <option value="phone">Phone</option>
+            </select>
+
+            <label for="message">Message: <span class="required">*</span></label>
+            <textarea id="message" name="message" placeholder="Enter your message here."></textarea>
+             
+            <input type="submit" value="Submit" id="submit-button" />
+            <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
         
-            {/*<form method="post" action="php/process.php">
-                      <label for="name">Name: <span class="required">*</span></label>
-                      <input type="text" id="name" name="name" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['name'] : '' ?>" placeholder="John Doe" required="required" <?php echo($sr && !$cr['form_ok'] ? 'autofocus="autofocus"' : '')?> />
-                       
-                      <label for="email">Email Address: <span class="required">*</span></label>
-                      <input type="email" id="email" name="email" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['email'] : '' ?>" placeholder="johndoe@example.com" required="required" />
-                       
-                      <label for="telephone">Telephone: </label>
-                      <input type="tel" id="telephone" name="telephone" placeholder="(780) 444 4444" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['telephone'] : '' ?>" />
-                       
-                      <label for="contactType">Preferred Mode of Contact: </label>
-                      <select id="contactType" name="contactType" >
-                          <option value="email" <?php echo($sr && !$cr['form_ok'] && $cf['posted_form_data']['contactType'] == 'email') ? "selected='selected'" : '' ?>>Email</option>
-                          <option value="phone" <?php echo($sr && !$cr['form_ok'] && $cf['posted_form_data']['contactType'] == 'phone') ? "selected='selected'" : '' ?>>Phone</option>
-                      </select>
-                       
-                      <label for="message">Message: <span class="required">*</span></label>
-                      <textarea id="message" name="message" placeholder="Enter your message here." required= data-minlength="4"><?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['message'] : '' ?></textarea>
-                       
-                      <input type="submit" value="Submit" id="submit-button" />
-                      <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
-            </form> */}
             </div>
           </div>
           </div>
