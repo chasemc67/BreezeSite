f1f057c2c977e298c28ef0f4c63d97fb30115a94
chasemc67 <chasemc67@gmail.com>
2017-01-07 19:08:08 -0700

    Contact component fields are wired to state now

--- src/ContactComponent.js
+++ src/ContactComponent.js
@@ -2,9 +2,55 @@ import React, { Component } from 'react';
 
 
 export default class ContactComponent extends Component {
-  submitEmail() {
-    
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      contactType: "",
+      message: ""
+    };
+
+    this.submitEmail = this.submitEmail.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleContactTypeChange = this.handleContactTypeChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
   }
+
+  submitEmail() {
+    console.log("name: " + this.state.name);
+    console.log("email: " + this.state.email);
+    console.log("phone: " + this.state.phone);
+    console.log("contactType: " + this.state.contactType);
+    console.log("message: " + this.state.message);
+  }
+
+  handleNameChange(event) {
+    console.log("name changed to: " + event.target.value);
+    this.setState({name: event.target.value});
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handlePhoneChange(event) {
+    this.setState({phone: event.target.value});
+  }
+
+  handleContactTypeChange(event) {
+    console.log("Contqact Type changed");
+    this.setState({contactType: event.target.value});
+  }
+
+  handleMessageChange(event) {
+    this.setState({message: event.target.value});
+  }
+  
 
   render() {
     return (
@@ -20,24 +66,24 @@ export default class ContactComponent extends Component {
             <div id="contact-form">
 
             <label for="name">Name: <span class="required">*</span></label>
-            <input type="text" id="name" name="name" placeholder="John Smith" value="" />
+            <input type="text" id="name" name="name" placeholder="John Smith" value={this.state.name} onChange={this.handleNameChange} />
 
             <label for="email">Email Address: <span class="required">*</span></label>
-            <input type="email" id="email" name="email" value="" placeholder="johnsmith@example.com" required="required" />
+            <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleEmailChange} placeholder="johnsmith@example.com" required="required" />
 
             <label for="telephone">Telephone: </label>
-            <input type="tel" id="telephone" name="telephone" placeholder="(780) 444 4444" value="" />
+            <input type="tel" id="telephone" name="telephone" placeholder="(780) 444 4444" value={this.state.phone} onChange={this.handlePhoneChange} />
 
             <label for="contactType">Preferred Mode of Contact: </label>
-            <select id="contactType" name="contactType" >
+            <select id="contactType" name="contactType" onChange={this.handleContactTypeChange} >
                 <option value="email">Email</option>
                 <option value="phone">Phone</option>
             </select>
 
             <label for="message">Message: <span class="required">*</span></label>
-            <textarea id="message" name="message" placeholder="Enter your message here."></textarea>
+            <textarea id="message" name="message" placeholder="Enter your message here." value={this.state.message} onChange={this.handleMessageChange}></textarea>
              
-            <input type="submit" value="Submit" id="submit-button" />
+            <input type="submit" value="Submit" id="submit-button" onClick={this.submitEmail} />
             <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
         
             </div>
