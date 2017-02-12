fd0a6f42a4dd7261b3701c7c1477a3ab234babbd
chasemc67 <chasemc67@gmail.com>
2017-02-11 19:09:35 -0700

    Testimonials almost being spaced properly

--- src/TestimonialsComponent.js
+++ src/TestimonialsComponent.js
@@ -1,5 +1,16 @@
 // http://kenwheeler.github.io/nuka-carousel/#/
 
+// https://blog.hubspot.com/marketing/testimonial-page-examples
+
+
+// Thinking 3 tall white rounded rects in the middle of the page
+// Circular picture out top, with name underneath, 
+// and then testimonial under that.
+
+// Hitting next button animates a slide to 3 new testimonies
+// So carousel wont be edge to edge, but rather contained in the
+// middle of the page (probably edge to edge on mobile though)
+
 import React, { Component } from 'react';
 import Carousel from 'nuka-carousel';
 
@@ -15,7 +26,9 @@ export default class TestimonialsComponent extends Component {
 		return(
 			<div className="testimony">
 				<img src={image} />
-				<h1>{text}</h1>
+				<div className="testimonyBody">
+					<p>{text}</p>
+				</div>
 			</div>
 		);
 	}
@@ -23,7 +36,11 @@ export default class TestimonialsComponent extends Component {
 	render() {
 		return(
 			<Carousel>
-				{this.getTestimony(TestFace, "Omg its my face")}
+				<div className="testimonyGroup">
+					{this.getTestimony(TestFace, "Omg its my face")}
+					{this.getTestimony(TestFace, "Omg its your face")}
+					{this.getTestimony(TestFace, "Omg its their face")}
+				</div>
 		        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
 		        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
 		        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
